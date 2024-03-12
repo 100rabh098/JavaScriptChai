7536e1bbbf50ab02e26f30e8fca81476774afb6a
Saurabh Singh <114741274+100rabh098@users.noreply.github.com>
2024-03-12 21:21:21 +0000

    done for datatypes and conversion

---
+++ basics/datatypesSummary.js
@@ -0,0 +1,32 @@
+//******Primitive
+
+//7 types: number,string,boolean,null,undefined,symbol,BigInt
+
+
+
+//*******Reference(Non Premitive)
+
+// array objects and functions
+
+const heroes=["shaktiman","nagraj","doga"]
+
+let myobj={
+    name: "saurabh",
+    age:20,
+}
+
+const myfunction= function(){
+    console.log("hello world");
+}
+
+// return data type
+/* premetive : 
+12=> number
+"12" =>string
+true=> boolean
+symbol=> symbol
+
+non premetive
+all return =>functions
+funtions=> object function
+*/

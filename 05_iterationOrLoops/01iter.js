a5869fa6e0003ee3b20b2d201c8a79075c5c4049
Saurabh Singh <114741274+100rabh098@users.noreply.github.com>
2024-03-14 18:10:48 +0000

    looping

---
+++ 05_iterationOrLoops/01iter.js
@@ -0,0 +1,21 @@
+
+//for
+
+for(let i=0;i< 10;i++)
+{
+    //console.log(i);
+}
+
+const superheroe= ["flash","IronMan","BlackPanther"]
+
+for (let index = 0; index < superheroe.length; index++) {
+    const element = superheroe[index];
+    //console.log(element)
+    
+}
+
+// break and continue
+
+//break breaks the loop and no further running of loop
+
+// continue continues after statement for that perticular iteration
---
+++ 05_iterationOrLoops/02_whileLoop.js
@@ -0,0 +1,25 @@
+
+
+//while loop
+
+let i=0
+while (i<=10) {
+    //console.log(i);
+    i+=2
+}
+
+
+let index=0;
+let myArray= ["sperman","batman","saurabhman"]
+
+while(index< myArray.length)
+{
+    console.log(myArray[index])
+    index+=1
+}
+
+//do while loop;
+
+// do {
+    
+// } while (condition);
